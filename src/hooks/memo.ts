e6c151294e1ed90a5fb8cf2471a1080c3351e7c6
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-12-17 00:11:58 +0900

    Change to Updater using Immer

--- src/hooks/memo.ts
+++ src/hooks/memo.ts
@@ -1,39 +1,49 @@
-import { useRecoilState } from 'recoil'
-import { memoListState } from '~/atoms/memo'
 import { useCallback } from 'react'
+import { useSetRecoilState } from 'recoil'
+import produce from 'immer'
+import { memoListState } from '~/atoms/memo'
 import { buildMemo } from '~/entities/memo'
 import { EntityId } from '~/entities/entity'
 
 export function useOperationMemo() {
-  const [memoList, setMemoList] = useRecoilState(memoListState)
+  const updater = useSetRecoilState(memoListState)
 
   return {
     insertMemo: useCallback(
       (content = '') => {
-        setMemoList([...memoList, buildMemo(content)])
+        updater((state) => {
+          return produce(state, (draft) => {
+            draft.push(buildMemo(content))
+          })
+        })
       },
-      [memoList, setMemoList]
+      [updater]
     ),
     updateMemo: useCallback(
       (id: EntityId, newContent: string) => {
-        const index = memoList.findIndex((item) => item.id === id)
-        setMemoList([
-          ...memoList.slice(0, index),
-          {
-            id,
-            content: newContent,
-          },
-          ...memoList.slice(index + 1),
-        ])
+        updater((state) => {
+          return produce(state, (draft) => {
+            const index = draft.findIndex((item) => item.id === id)
+            if (index > -1) {
+              draft[index].content = newContent
+            }
+          })
+        })
       },
-      [memoList, setMemoList]
+      [updater]
     ),
     removeMemo: useCallback(
       (id: EntityId) => {
-        const index = memoList.findIndex((item) => item.id === id)
-        setMemoList([...memoList.slice(0, index), ...memoList.slice(index + 1)])
+        updater((state) => {
+          return produce(state, (draft) => {
+            const index = draft.findIndex((item) => item.id === id)
+            if (index > -1) {
+              draft.splice(index, 1)
+            }
+          })
+        })
       },
-      [memoList, setMemoList]
+      [updater]
     ),
   }
 }
